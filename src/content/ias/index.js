898015057aea008278f1fd72689d86663eb2a167
Тимур Кадыров <kadyrovt405@gmail.com>
2026-08-04 23:23:23 +0500

    Добавил практическое руководство по IAS 16

--- src/content/ias/index.js
+++ src/content/ias/index.js
@@ -1,7 +1,9 @@
 import { ias2Content } from "./ias2.js";
 import { ias7Content } from "./ias7.js";
+import { ias16Content } from "./ias16.js";
 
 export const iasContentById = {
   2: ias2Content,
   7: ias7Content,
+  16: ias16Content,
 };

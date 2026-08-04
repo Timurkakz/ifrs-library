45fb3416b97cd18cc3a539ec0b954c836728517f
Тимур Кадыров <kadyrovt405@gmail.com>
2026-08-04 23:41:35 +0500

    Добавил практическое руководство по IAS 21

--- src/content/ias/index.js
+++ src/content/ias/index.js
@@ -2,6 +2,7 @@ import { ias2Content } from "./ias2.js";
 import { ias7Content } from "./ias7.js";
 import { ias12Content } from "./ias12.js";
 import { ias16Content } from "./ias16.js";
+import { ias21Content } from "./ias21.js";
 import { ias36Content } from "./ias36.js";
 import { ias37Content } from "./ias37.js";
 import { ias38Content } from "./ias38.js";
@@ -11,6 +12,7 @@ export const iasContentById = {
   7: ias7Content,
   12: ias12Content,
   16: ias16Content,
+  21: ias21Content,
   36: ias36Content,
   37: ias37Content,
   38: ias38Content,

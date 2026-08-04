d2d25d91e1843a93e2e93e7a96ecdb6abc6ddcdb
Тимур Кадыров <kadyrovt405@gmail.com>
2026-08-04 23:34:54 +0500

    Добавил практическое руководство по IAS 38

--- src/content/ias/index.js
+++ src/content/ias/index.js
@@ -3,6 +3,7 @@ import { ias7Content } from "./ias7.js";
 import { ias16Content } from "./ias16.js";
 import { ias36Content } from "./ias36.js";
 import { ias37Content } from "./ias37.js";
+import { ias38Content } from "./ias38.js";
 
 export const iasContentById = {
   2: ias2Content,
@@ -10,4 +11,5 @@ export const iasContentById = {
   16: ias16Content,
   36: ias36Content,
   37: ias37Content,
+  38: ias38Content,
 };

3b5deb507fd9feb7f1bdfca83d82a3c10cefbf3d
Тимур Кадыров <kadyrovt405@gmail.com>
2026-08-04 23:38:26 +0500

    Добавил практическое руководство по IAS 12

--- src/content/ias/index.js
+++ src/content/ias/index.js
@@ -1,5 +1,6 @@
 import { ias2Content } from "./ias2.js";
 import { ias7Content } from "./ias7.js";
+import { ias12Content } from "./ias12.js";
 import { ias16Content } from "./ias16.js";
 import { ias36Content } from "./ias36.js";
 import { ias37Content } from "./ias37.js";
@@ -8,6 +9,7 @@ import { ias38Content } from "./ias38.js";
 export const iasContentById = {
   2: ias2Content,
   7: ias7Content,
+  12: ias12Content,
   16: ias16Content,
   36: ias36Content,
   37: ias37Content,

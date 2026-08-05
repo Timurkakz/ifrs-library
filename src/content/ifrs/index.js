488064f30306a32c69f7ee7dd9df9c64bfe0bc1b
Тимур Кадыров <kadyrovt405@gmail.com>
2026-08-05 23:13:35 +0500

    Добавил практическое руководство по IFRS 13

--- src/content/ifrs/index.js
+++ src/content/ifrs/index.js
@@ -1,15 +1,17 @@
 import { ifrs1Content } from "./ifrs1.js";
 import { ifrs4Content } from "./ifrs4.js";
 import { ifrs9Content } from "./ifrs9.js";
+import { ifrs13Content } from "./ifrs13.js";
 import { ifrs15Content } from "./ifrs15.js";
 import { ifrs16Content } from "./ifrs16.js";
 import { ifrs17Content } from "./ifrs17.js";
 import { ifrs18Content } from "./ifrs18.js";
 
 export const ifrsContentById = {
- 1: ifrs1Content,
+ 1: ifrs1Content,   
     4: ifrs4Content,
     9: ifrs9Content,
+    13: ifrs13Content,
   15: ifrs15Content,
   16: ifrs16Content,
   17: ifrs17Content,
